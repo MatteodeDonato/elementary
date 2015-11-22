fa41abb43f4b90c17d0ff8208706a95277e48ad4
Matteo de Donato <MatteodeDonato@users.noreply.github.com>
2015-11-22 15:22:53 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,63 @@
+var cvs, ctx;
+var elements = [];
+var elements2 = [];
+function init() {
+   cvs = document.getElementById("periodicTable");
+   ctx = cvs.getContext("2d");
+   ctx.strokeStyle = 'black';
+   drawImg();
+}
+function drawImg() {
+  var count = 0;
+  for(var j = 0; j < 10; j++){
+    for(var i = 0; i < 18; i++){
+        if(j === 0 && i > 0 && i < 17)
+          continue;
+        else if (j > 0 && j < 3 && i > 1 && i < 12)
+          continue;
+        else if (i == 2 && j > 4 && j<7)
+          continue;
+        else if (j == 7)
+          continue;
+        else if (j > 7 && j < 10 && i >= 0 && i < 3)
+          continue;
+        else {
+          count++;
+          if(j == 5 && i > 2)
+            elements.push(new Element(count + 15,i*44+75, j*44+50));
+          else if(j == 8 && i > 2)
+              elements.push(new Element(count - 32,i*44+75, j*44+50));
+          else if(j == 6 && i < 2)
+              elements.push(new Element(count + 15,i*44+75, j*44+50));
+          else if(j == 6 && i > 1)
+              elements.push(new Element(count + 30,i*44+75, j*44+50));
+          else if(j == 9)
+               elements.push(new Element(count - 15,i*44+75, j*44+50));
+          else
+            elements.push(new Element(count,i*44+75, j*44+50));
+        }
+     }
+  }
+  for(var m = 1; m < 119; m++){
+    for(var n = 0; n < elements.length; n++){
+      if(elements[n].number == m)
+        elements2.push(elements[n]);
+    }
+  }
+  elements = elements2;
+
+  for(var x = 0; x < elements.length; x++){
+    ctx.moveTo(elements[x].posx+20,elements[x].posy);
+    ctx.arc(elements[x].posx, elements[x].posy, 20, 0, 2*Math.PI);
+    ctx.stroke();
+    ctx.fillText(elements[x].number,elements[x].posx, elements[x].posy);
+    console.log(elements[x].number);
+  }
+}
+
+function Element(number, posx, posy) {
+    this.number = number;
+    this.posx = posx;
+    this.posy = posy;
+}
+window.onload = init;
